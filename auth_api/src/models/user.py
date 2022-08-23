25f0a6d68451f1182294436c23d18c0c64e5784e
IAmIrina <irina.vl.kiseleva@gmail.com>
2022-08-23 16:16:39 +0000

    Add social_account table

--- auth_api/src/models/user.py
+++ auth_api/src/models/user.py
@@ -31,10 +31,26 @@ class UserPersonalData(db.Model):
     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'), unique=True)
     first_name = db.Column(db.String)
     second_name = db.Column(db.String)
-    email = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
 
     def __repr__(self):
         return f'User details {self.first_name} {self.second_name}'
+
+
+class SocialAccount(db.Model):
+    __tablename__ = 'social_account'
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True)
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship(User, backref=db.backref('social_accounts', lazy=True))
+
+    social_id = db.Column(db.Text, nullable=False)
+    social_name = db.Column(db.Text, nullable=False)
+
+    __table_args__ = (db.UniqueConstraint('social_id', 'social_name', name='social_pk'), )
+
+    def __repr__(self):
+        return f'<SocialAccount {self.social_name}:{self.user_id}>'
 
 
 class UserHistory(db.Model):

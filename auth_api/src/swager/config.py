5b343e22a9f4da29b933dd8e386cdb4b53c12809
Alex Shvedov <alexandershvedov1997@gmail.com>
2022-09-04 12:46:32 +0300

    fix docs for social auth

--- auth_api/src/swager/config.py
+++ auth_api/src/swager/config.py
@@ -5,12 +5,8 @@ from swager.roles import (
   roles_by_user_id,
   roles_by_user_id_and_role_id,
 )
-from swager.oauth import (
-  oauth2_mail_login,
-  oauth2_yandex_login,
-  oauth2_vk_login,
-  oauth2_google_login,
-)
+from swager.oauth import socials_url
+
 
 TEMPLATE = {
   "swagger": "2.0",
@@ -33,45 +29,13 @@ TEMPLATE = {
         "description": "Secret internal key to communicate microservice"
     },
     "BasicAuth": {"type": "basic"},
-    "OAuth2Mail": {
+    "OAuth2Auth": {
       "type": "oauth2",
-      "description": "OAuth2 mail",
+      "description": "OAuth2",
       "flow": "accessCode",
-      "authorizationUrl": "https://oauth.mail.ru/login",
-      "tokenUrl": "https://oauth.mail.ru/token",
+      "authorizationUrl": 'social authorize url',
+      "tokenUrl": 'social authorize token',
       "scopes": {
-              "userinfo": "Get information about user"
-      }
-    },
-   "OAuth2Yandex": {
-      "type": "oauth2",
-      "description": "OAuth2 yandex",
-      "flow": "accessCode",
-      "authorizationUrl": 'https://oauth.yandex.ru/authorize',
-      "tokenUrl": 'https://oauth.yandex.ru/token',
-      "scopes": {
-              "login:info": "Get info about user",
-              "login:email": "Get user emails",
-      }
-    },
-    "OAuth2Vk": {
-      "type": "oauth2",
-      "description": "OAuth2 vk",
-      "flow": "accessCode",
-      "authorizationUrl": 'https://oauth.vk.com/authorize',
-      "tokenUrl": 'https://oauth.vk.com/access_token',
-      "scopes": {
-              "email": "Get user email",
-      }
-    },
-    "OAuth2Google": {
-      "type": "oauth2",
-      "description": "OAuth2 google",
-      "flow": "accessCode",
-      "authorizationUrl": 'https://accounts.google.com/o/oauth2/auth',
-      "tokenUrl": 'https://accounts.google.com/o/oauth2/token',
-      "scopes": {
-              "openid": "OpenID",
               "email": "Get user email",
               "profile": "Get user profile",
       }
@@ -88,9 +52,6 @@ TEMPLATE = {
       "/roles": roles,
       "/users/{user_id}/roles/{role_id}": roles_by_user_id_and_role_id,
       "/users/{user_id}/roles": roles_by_user_id,
-      "/mail/login": oauth2_mail_login,
-      "/yandex/login": oauth2_yandex_login,
-      "/vk/login": oauth2_vk_login,
-      "/google/login": oauth2_google_login,
+      "/socials/login/{name}": socials_url,
   }
 }
--- auth_api/src/swager/oauth.py
+++ auth_api/src/swager/oauth.py
@@ -1,79 +1,26 @@
-oauth2_mail_login = {
-        "get": {
-            "summary": "Авторизация через oauth2 mail",
-            "tags": ["oauth2"],
-            "security": [
-                {
-                    "OAuth2Mail": []
-                }
-            ],
-            "responses": {
-                "302": {
-                    "description": "Redirect url /mail/authorize",
-                        "schema": {
-                            "$ref": "#/definitions/Tokens"
-                    }
+socials_url = {
+    "get": {
+        "summary": "Авторизация через oauth2 google",
+        "tags": ["oauth2"],
+        "parameters": [
+            {
+                    "in": "path",
+                    "name": "name",
+                    "schema": {"type": "string"},
+                    "description": "Require one of tehe next socials: yandex, google, mail, vk"
+            }],
+        "security": [
+            {
+                "OAuth2Auth": []
+            }
+        ],
+        "responses": {
+            "200": {
+                "description": "Oauth2 path",
+                "schema": {
+                    "$ref": "#/definitions/Tokens"
                 }
             }
         }
     }
-
-oauth2_yandex_login = {
-        "get": {
-            "summary": "Авторизация через oauth2 yandex",
-            "tags": ["oauth2"],
-            "security": [
-                {
-                    "OAuth2Yandex": []
-                }
-            ],
-            "responses": {
-                "302": {
-                    "description": "Redirect url /yandex/authorize",
-                        "schema": {
-                            "$ref": "#/definitions/Tokens"
-                    }
-                }
-            }
-        }
-    }
-
-oauth2_vk_login = {
-        "get": {
-            "summary": "Авторизация через oauth2 vk",
-            "tags": ["oauth2"],
-            "security": [
-                {
-                    "OAuth2Vk": []
-                }
-            ],
-            "responses": {
-                "302": {
-                    "description": "Redirect url /vk/authorize",
-                        "schema": {
-                            "$ref": "#/definitions/Tokens"
-                    }
-                }
-            }
-        }
-    }
-
-oauth2_google_login = {
-        "get": {
-            "summary": "Авторизация через oauth2 google",
-            "tags": ["oauth2"],
-            "security": [
-                {
-                    "OAuth2Google": []
-                }
-            ],
-            "responses": {
-                "302": {
-                    "description": "Redirect url /google/authorize",
-                        "schema": {
-                            "$ref": "#/definitions/Tokens"
-                    }
-                }
-            }
-        }
-    }
+}
